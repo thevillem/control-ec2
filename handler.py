e804d911a58a740b39db301a79cdd68dda12a783
Mike V <mike.villen@gmail.com>
2021-03-07 18:51:11 -0500

    Initial project commit.

---
+++ handler.py
@@ -0,0 +1,22 @@
+import datetime
+import logging
+import boto3
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+instances = ['i-0c548e60cd4c5592e']
+ec2 = boto3.client('ec2', region_name="us-east-1")
+
+def start(event, context):
+    current_time = datetime.datetime.now().time()
+    ec2.start_instances(InstanceIds=instances)
+    logger.info(f"started instances: {instances}")
+    name = context.function_name
+    logger.info(f"Your cron function {name} ran at {current_time}")
+
+def stop(event, context):
+    current_time = datetime.datetime.now().time()
+    ec2.stop_instances(InstanceIds=instances)
+    logger.info(f"stopped instances: {instances}")
+    name = context.function_name
+    logger.info(f"Your cron function {name} ran at {current_time}")
